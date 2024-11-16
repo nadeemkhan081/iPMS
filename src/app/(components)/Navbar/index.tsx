d83c698bb3b2bb746e9e23bf873d9716c2abc4a5
MOHAMMAD NADEEM <mnadeem.mn838@gmail.com>
2024-11-16 18:59:03 +0530

    darkmode feature added

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { Menu, Search, Settings } from "lucide-react";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/state";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
   );
-  const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
-  
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* SearchBar */}
       <div className="flex items-center gap-8">
         {!isSidebarCollapsed ? null : (
-          <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
             <Menu className="h-8 w-8 dark:text-white" />
           </button>
         )}
@@ -33,8 +35,27 @@ const Navbar = () => {
       {/* Icons  */}
 
       <div className="flex items-center">
-        <Link href="/settings" className=" h-min w-min rounded p-2 hover:bg-gray-100">
-           <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >{isDarkMode ? (
+          <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+        ) : (
+          <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+        )}</button>
+        <Link
+          href="/settings"
+          className={
+            isDarkMode
+              ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
+          }
+        >
+          <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
         </Link>
         <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
       </div>
